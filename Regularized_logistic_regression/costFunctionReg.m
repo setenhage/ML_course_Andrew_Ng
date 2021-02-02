a891b52fa1335398bcfbe40c4b101101f8e60b1e
setenhage <suzanne.tenhage@gmail.com>
2021-02-01 18:25:36 -0800

    Completed regularized logistic regression assignment

--- Regularized_logistic_regression/costFunctionReg.m
+++ Regularized_logistic_regression/costFunctionReg.m
@@ -16,11 +16,22 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+n = length(theta);
 
+%hypothesis
+hx = sigmoid(X * theta);
 
+%cost function
+J = -(1/m) * sum(y .* log(hx) + (ones(size(m,1)) - y) .* ...
+    log(ones(m,1) - hx)) + 0.5 * (lambda/m) * sum(theta(2:n).^2) ;
 
+%Gradient for theta(0)
+grad(1) = (1/m) * sum((hx - y)' * X(:,1));
 
-
+%Gradient for theta(1 ... n)
+for i = 2:length(theta)
+    grad(i) = (1/m) * sum((hx - y)' * X(:,i)) + lambda/m * sum(theta(i));
+end
 
 % =============================================================
 
