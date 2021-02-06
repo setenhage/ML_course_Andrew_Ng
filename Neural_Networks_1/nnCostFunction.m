9dfcc9863dc2e40520a9792cee2b822098cf20ef
setenhage <suzanne.tenhage@gmail.com>
2021-02-05 17:18:13 -0800

    Added regularization

--- Neural_Networks_1/nnCostFunction.m
+++ Neural_Networks_1/nnCostFunction.m
@@ -67,6 +67,9 @@ hx = sigmoid(alpha2 * Theta2');
 J = -(1/m) *  sum(sum(yc' .* log(hx) + (ones(size(yc)) - yc)' .* ...
     log(ones(size(yc))' - hx),2));
 
+%comput cost regularized
+J = J + (lambda/(2*m)) * (sum(sum(Theta1(:,2:end).^2)) + ...
+    sum(sum((Theta2(:,2:end).^2))));
 
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
