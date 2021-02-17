9502c9d68d7d37ded6626e018fba80f3ee4d1ebe
setenhage <suzanne.tenhage@gmail.com>
2021-02-17 11:40:04 -0800

    SVM with Gaussian Kernel and grid search

--- Support_vector_Machines/dataset3Params.m
+++ Support_vector_Machines/dataset3Params.m
@@ -22,12 +22,25 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+error = zeros(8,8);
+Cvals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+Svals = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3];
 
+for i = 1:length(Cvals)
+    CTest = Cvals(i);
+    for j = 1:length(Svals)
+        sigmaTest = Svals(j);
+        model = svmTrain(X, y, CTest, @(x1, x2)gaussianKernel(x1, x2, sigmaTest));
+        pred = svmPredict(model, Xval);
+        error(i,j) = mean(double(pred ~= yval));
+        
+    end
+end
 
-
-
-
-
+min_error = min(error, [], 'all');
+[row column] = find(error == min_error);
+C = Cvals(row);
+sigma = Svals(column);
 
 % =========================================================================
 
